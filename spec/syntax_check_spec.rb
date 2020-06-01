429daa1105ed8b4ff9c8bc1deb0d047680572ea7
Christopher Amanor <kriskit7@gmail.com>
2020-06-01 22:50:27 +0000

    Add Rspec test for SyntaxCheck module and fixed rubocop errors

--- spec/syntax_check_spec.rb
+++ spec/syntax_check_spec.rb
@@ -1,11 +1,21 @@
 require_relative '../lib/syntax_check.rb'
+require_relative '../lib/censor.rb'
 
 describe SyntaxCheck do
-  describe '#something something' do
-    it 'returns true if ' do
+  let(:check) { Censor.new }
+  let(:line1) { ["padding: 10px\n"] }
+  let(:line2) { ["padding: 10px;\n"] }
+  let(:errors) { [] }
+
+  describe 'missing_semicolon_check' do
+    it 'returns error message if there is no semi-colon at the end of the line' do
+      expect(check.missing_semicolon_check(line1, errors))
+        .to eql(["Line does not end with ';' on line 1."])
     end
 
-    it 'returns false if ' do
+    it 'returns an empty array if there is a semi-colon at the end of the line ' do
+      expect(check.missing_semicolon_check(line2, errors))
+        .to eql([])
     end
   end
 end
