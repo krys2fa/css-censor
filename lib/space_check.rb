7bd471574b93d1e8100a53aee1e4afff1fe7cfc8
Christopher Amanor <kriskit7@gmail.com>
2020-06-01 21:47:22 +0000

    Fix rubocop errors

--- lib/space_check.rb
+++ lib/space_check.rb
@@ -2,18 +2,19 @@ module SpaceCheck
   # rubocop:disable Metrics/CyclomaticComplexity
   def self.trailing_space_check(lines, errors)
     lines.each_with_index do |line, index|
-      errors << "  Error: Trailing space found on line #{index + 1}." if line.end_with?("; \n")
+      errors << "Trailing space found on line #{index + 1}." if line.end_with?("; \n")
     end
     errors
   end
 
   def self.indentation_check(lines, errors)
     lines.each_with_index do |line, index|
-      next if line.start_with?('@') || line.include?('{') || line.include?('}') || line == "\n"
+      next if line.start_with?('@') || line == "\n" || line.end_with?(",\n")
+      next if ['{', '}'].any? { |needle| line.include? needle }
 
       num_of_spaces = line[/\A */].size
       if num_of_spaces < 2 || num_of_spaces > 2
-        errors << "  Error: Indentation of 2 spaces expected. \
+        errors << "Indentation of 2 spaces expected.
           Found #{num_of_spaces} spaces instead on line #{index + 1}."
       end
     end
@@ -24,14 +25,12 @@ module SpaceCheck
     lines.each_with_index do |line, index|
       next unless line.include?('{')
 
-      unless line =~ /[[:space:]]\{/
-        errors << "  Error: There should be a space before curly bracket on line #{index + 1}"
-      end
+      errors << "There should be a space before curly bracket on line #{index + 1}" unless line =~ /[[:space:]]\{/
     end
   end
 
   def self.last_line_check(lines, errors)
-    errors << '  Error: Last line in the file should be empty.' unless lines.last.include? "\n"
+    errors << 'Last line in the file should be empty.' unless lines.last.include? "\n"
   end
   # rubocop:enable Metrics/CyclomaticComplexity
 end
